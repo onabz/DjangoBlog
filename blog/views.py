448fd0d425bf54918150c3efc564d1785b480cc3
onabz <ONABOLUMBE@GMAIL.COM>
2022-08-04 10:54:42 +0000

    commit

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
-
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
